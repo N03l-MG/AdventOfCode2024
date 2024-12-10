a4d541de9c0035b2ac6155b8562bbf63971f3266
N03l-MG <noelmonzongarcia@gmail.com>
2024-12-10 22:54:36 +0100

    Day 10 Part 2

--- Day10.cs
+++ Day10.cs
@@ -37,11 +37,25 @@ public class Day10
 			}
 		}
 
+		// Part 2
+		int result2 = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (topographicMap[i, j] == 0) {
+                    int rating = CountTrails(topographicMap, rows, cols, new Pos(i, j), []);
+                    result2 += rating;
+                }
+            }
+        }
+
 		// Results
 		Console.WriteLine("Part 1: " + result1);
+		Console.WriteLine("Part 2: " + result2);
 	}
 
-	// Helper for performing BFS to find trails and their scores
+	// Helper function doing BFS to find trails and their scores for Part 1
 	static int GetTrailheadScore(int[,] map, int rows, int cols, Pos start)
 	{
 		bool[,] visited = new bool[rows, cols];
@@ -69,5 +83,26 @@ public class Day10
 			}
 		}
 		return reachableNines.Count;
+	}
+
+	// Recursive BFS helper function to count unique trails for Part 2
+	static int CountTrails(int[,] map, int rows, int cols, Pos postion, HashSet<Pos> visited)
+	{
+		if (map[postion.R, postion.C] == 9) return 1;
+		visited.Add(postion);
+		int trailCount = 0;
+		for (int d = 0; d < 4; d++)
+		{
+			int newR = postion.R + move[d, 0];
+			int newC = postion.C + move[d, 1];
+			if (newR >= 0 && newR < rows && newC >= 0 && newC < cols) {
+				int currentHeight = map[postion.R, postion.C];
+				int nextHeight = map[newR, newC];
+				Pos nextPos = new(newR, newC);
+				if (!visited.Contains(nextPos) && nextHeight == currentHeight + 1)
+					trailCount += CountTrails(map, rows, cols, nextPos, [.. visited]);
+			}
+		}
+		return trailCount;
 	}
 }
